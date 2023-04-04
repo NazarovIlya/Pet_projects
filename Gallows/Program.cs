1b735be08ea5c6cbc1a5dd5f2f482da38f567b21
NazarovIlya <eliakohai@gmail.com>
2023-04-05 02:27:21 +0300

    main method added

--- Gallows/Program.cs
+++ Gallows/Program.cs
@@ -1,7 +1,20 @@
-﻿/* 1.При старте, приложение предлагает начать новую игру или выйти из приложения
+﻿/* 1. При старте, приложение предлагает начать новую игру или выйти из приложения
  * 2. При начале новой игры, случайным образом загадывается слово,
  * и игрок начинает процесс по его отгадыванию
  * 3. После каждой введенной буквы выводим в консоль счётчик ошибок,
  * текущее состояние виселицы (нарисованное ASCII символами)
- * 4.По завершении игры выводим результат (победа или поражение) и возвращаемся к состоянию #1, 
+ * 4. По завершении игры выводим результат (победа или поражение) и возвращаемся к состоянию #1, 
  * то есть предложению начать новую игру или выйти из приложения.*/
+
+using Gallows;
+
+Words words = new Words();
+Console.WriteLine(words.Word);
+Console.WriteLine(words.Word);
+Console.WriteLine(words.Word); 
+Console.WriteLine(words.Word);
+Console.WriteLine(words.Word);
+Console.WriteLine(words.Word);
+
+Console.WriteLine(words.GetEncodingWord("fddgwgdmv"));
+Console.WriteLine(words.GetUpdatedWord("fddgwgdmv", words.GetEncodingWord("fddgwgdmv"), 'd'));
