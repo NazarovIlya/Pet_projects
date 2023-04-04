5f35cfc8c86c7c3784d2ffa410a4406c719da9fb
NazarovIlya <eliakohai@gmail.com>
2023-04-05 00:42:53 +0300

    view & words model classes init

---
+++ Gallows/View.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Gallows
+{
+	internal class View
+	{
+		public void Menu(List<ICommand> commands) { }
+		public void GetChar(string? invite) { }
+		public void ShowWord(string word) { }
+	}
+}
---
+++ Gallows/Words.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallows
+{
+	internal class Words
+	{
+		private string word;
+		private string[] wordsArray;
+
+		public Words()
+		{
+			this.word = string.Empty;
+			FillWordsArray();
+		}
+		public string Word { get => word = GetWord(); private set => Word = word; }
+		private void FillWordsArray()
+		{
+			this.wordsArray = new string[]
+			{
+				"word 1",
+				"word 2"
+			};
+		}
+		private string GetWord() => wordsArray[Random.Shared.Next(wordsArray.Length)];
+	}
+}
