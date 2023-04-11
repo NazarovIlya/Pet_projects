1b6544449c90f5f636a7b79ce5af77db7431ed55
NazarovIlya <eliakohai@gmail.com>
2023-04-12 02:23:46 +0300

    controller init

---
+++ Gallows/Game.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallows
+{
+	internal class Game
+	{
+		public Game() { }
+
+		public void StartGame()
+		{
+			Render render = new Render(0, 0, 12);
+
+			Words words = new Words('*');
+			Console.WriteLine(words.Word);
+			Console.WriteLine(words.Word);
+			Console.WriteLine(words.Word);
+			Console.WriteLine(words.Word);
+			Console.WriteLine(words.Word);
+			Console.WriteLine(words.Word);
+
+			string word = "fddgwgdmv";
+
+			render.Draw(1);
+
+			string current = words.GetEncodingWord(word);
+			Console.WriteLine(current);
+
+			current = words.GetUpdatedWord(word, current, 'd');
+			Console.WriteLine(current);
+
+			current = words.GetUpdatedWord(word, current, 'g');
+			Console.WriteLine(current);
+		}
+	}
+}
