7ec5540ca79ca38278f86bb19659d822718321b9
NazarovIlya <eliakohai@gmail.com>
2023-04-19 18:20:50 +0300

    view interface added

---
+++ Gallows/IView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Gallows
+{
+	internal interface IView
+	{
+		public void Menu(List<ICommand> commands);
+		public char GetChar();
+		public void ShowWord(string word);
+		public void WriteSymbol(string symbol);
+		public void WriteCongratulations();
+		public void WriteGameOver();
+	}
+}
