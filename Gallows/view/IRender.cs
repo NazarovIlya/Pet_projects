a67fd7a4f06a8efd4accad286460bf26a4bfcba1
NazarovIlya <eliakohai@gmail.com>
2023-04-22 15:37:55 +0300

    folder strucure created

---
+++ Gallows/view/IRender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallows.view
+{
+	internal interface IRender
+	{
+		public void Draw();
+	}
+}
