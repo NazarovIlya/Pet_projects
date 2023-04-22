ca23640b1b005df28b97d04fba4980def578f19c
NazarovIlya <eliakohai@gmail.com>
2023-04-22 21:07:14 +0300

    full refactored of presenter

---
+++ Gallows/presenter/Presenter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallows.model
+{
+	internal class Presenter
+	{
+		public void StartGame()
+		{
+			IConfigDTO configDTO = new AppConfigDTO();
+
+			Game game = new Game(configDTO, new DictionaryService(), new ConsoleView());
+
+			game.StartGame();
+		}
+	}
+}
