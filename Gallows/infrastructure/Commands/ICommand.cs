150519f1287fc04a4cd475f7651a20316bbcf195
NazarovIlya <eliakohai@gmail.com>
2023-04-23 13:28:56 +0300

    command interface and classes realized

---
+++ Gallows/infrastructure/Commands/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallows.infrastructure.Commands
+{
+	internal interface ICommand
+	{
+		void Execute(Game game);
+		string Discription();
+	}
+}
---
+++ Gallows/infrastructure/Commands/QuiteCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallows.infrastructure.Commands
+{
+	internal class QuiteCommand : ICommand
+	{
+		public string Discription() => "Select and push to quite game.";
+
+		public void Execute(Game game) => Environment.Exit(0);
+	}
+}
