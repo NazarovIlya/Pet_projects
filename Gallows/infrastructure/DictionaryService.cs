39c14d2250657c0de56225e0899cbca4d4dc73fc
NazarovIlya <eliakohai@gmail.com>
2023-04-26 19:09:30 +0300

    dictionary updated

--- Gallows/infrastructure/DictionaryService.cs
+++ Gallows/infrastructure/DictionaryService.cs
@@ -6,35 +6,42 @@ using System.Threading.Tasks;
 
 namespace Gallows
 {
-	internal class DictionaryService : IDictionary
-	{
-		public string[] GetWordsArray(string fileName, int wordLength, int minWordsCount)
-		{
-			string[] inputWords = null;
-			string[] words = null;
-			FileInfo fileInfo = new FileInfo(fileName);
-			if (fileInfo.Exists)
-			{
-				inputWords = File.ReadAllLines(fileInfo.FullName);
-			}
-			words = (from word in inputWords
-					 where word.Length >= wordLength
-					 select word).ToArray();
+  internal class DictionaryService : IDictionary
+  {
+    public string[] GetWordsArray(string fileName, int wordLength, int minWordsCount)
+    {
+      string[] inputWords = null;
+      string[] words = null;
+      FileInfo fileInfo = new FileInfo(fileName);
+      if (fileInfo.Exists)
+      {
+        inputWords = File.ReadAllLines(fileInfo.FullName);
+      }
+      words = (from word in inputWords
+               where word.Length >= wordLength
+               select word).ToArray();
 
-			if (words.Length >= minWordsCount)
-				return words;
-			else
-			{
-				words = new string[]
-				{
-				"word 123",
-				"word 2",
-				"word 3",
-				"word 4",
-				"word 5"
-				};
-			}
-			return words;
-		}
-	}
+      if (words.Length >= minWordsCount)
+        return words;
+      else
+      {
+        words = new string[]
+        {
+        "ананас",
+        "абракадабра",
+        "абсорбент",
+        "авитаминоз",
+        "больница",
+        "бормотуха",
+        "бриллиант",
+        "бродяжничество",
+        "бронза",
+        "бронхиола",
+        "бронхит",
+        "бубен"
+        };
+      }
+      return words;
+    }
+  }
 }
